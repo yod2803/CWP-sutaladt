ecf263850b162f009a858252b84d3ed1f661085a
kali <you@example.com>
2026-09-22 17:46:22 +0700

    KEKE

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 def main():
     original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    
     modified_array = {x + 2 for x in original_array if x > 5}
 
     print(original_array)
--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -2,7 +2,6 @@
 import sys
 
 def main():
-    # sys.argv[0] is the script name. If len is > 1, at least one parameter was passed.
     if len(sys.argv) > 1:
         print(sys.argv[1])
     else:
